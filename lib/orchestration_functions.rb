cf34fb5a09a779e6d628f64155aad30fc027816f
Ethan Miller <ethan.w.miller@gmail.com>
2017-12-12 11:15:17 -0500

    verified the json is being passed correctly

--- lib/orchestration_functions.rb
+++ lib/orchestration_functions.rb
@@ -9,13 +9,15 @@ end
 
 def process_json(json_body)
   puts("received json body")
-  
+
   data = JSON.parse(json_body)
   firewall_uri = URI('http://localhost:8080/api/firewall')
   dns_uri = URI('http://localhost:8081/api/dns')
 
   firewall_data = data["microservices"]["firewall"].to_json
+  puts("firewall data: " + firewall_data)
   dns_data = data["microservices"]["dns"].to_json
+  puts("dns data: " + dns_data)
 
   send_request(firewall_uri, firewall_data)
   send_request(dns_uri, dns_data)
--- workflow_proxy.rb
+++ workflow_proxy.rb
@@ -1,11 +1,12 @@
 require 'sinatra'
 require_relative './lib/orchestration_functions.rb'
 
-get '/' do 
+get '/' do
   'Put this in your pipe and orchestrate it!'
 end
 
 post '/api/v2/orchestration' do
   'Received POST request'
-  process_json(request.body.read)
+  request_body = request.body.read
+  process_json(request_body)
 end
